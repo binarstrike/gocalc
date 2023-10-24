9df44ab06d12c33c5610dd849b9176e188496755
Binar Nugroho <binarnugroho775@gmail.com>
2023-10-24 17:11:45 +0700

    implement better error handling
    - add more type to type parameter
    - change method name from Sums() to Results()

--- gocalc.go
+++ gocalc.go
@@ -1,8 +1,23 @@
 package gocalc
 
+type Operation string
+
+const (
+	Plus     Operation = "Plus()"
+	Min      Operation = "Min()"
+	Multiply Operation = "Multiply()"
+	Divide   Operation = "Divide()"
+)
+
 type IChainable[T any] interface {
 	IChainableOperation[T]
-	Sums() T
+	Result() T
+	Error() error
+	IsError() bool
+	ResultAndError() (T, error)
+	LastSuccessOperation() Operation
+	LastErrorOperation() Operation
+	LastOperation() Operation
 }
 
 type IChainableOperation[T any] interface {
@@ -12,12 +27,63 @@ type IChainableOperation[T any] interface {
 	Divide(T) IChainable[T]
 }
 
-type Chainable[T int | uint | float64] struct {
-	value T
+type Chainable[T int | int8 | int16 | int32 | int64 | uint | uint8 |
+	uint16 | uint32 | uint64 | float32 | float64 | complex64 | complex128] struct {
+	sums                 T
+	lastErrorOperation   Operation
+	lastSuccessOperation Operation
+	lastOperation        Operation
+	lastErrorCaught      error
 }
 
-func New[T int | uint | float64](value T) IChainable[T] {
+func New[T int | int8 | int16 | int32 | int64 | uint | uint8 |
+	uint16 | uint32 | uint64 | float32 | float64 | complex64 | complex128](startValue T) IChainable[T] {
 	c := new(Chainable[T])
-	c.value = value
+	c.sums = startValue
+	c.lastOperation = ""
+	c.lastSuccessOperation = ""
+	c.lastErrorOperation = ""
+	c.lastErrorCaught = nil
+	return c
+}
+
+func (c Chainable[T]) Result() T {
+	return c.sums
+}
+
+func (c Chainable[T]) Error() error {
+	return c.lastErrorCaught
+}
+
+func (c Chainable[T]) IsError() bool {
+	return c.Error() != nil
+}
+
+func (c Chainable[T]) ResultAndError() (T, error) {
+	return c.sums, c.lastErrorCaught
+}
+
+func (c Chainable[T]) LastSuccessOperation() Operation {
+	return c.lastSuccessOperation
+}
+
+func (c Chainable[T]) LastOperation() Operation {
+	return c.lastOperation
+}
+
+func (c Chainable[T]) LastErrorOperation() Operation {
+	return c.lastErrorOperation
+}
+
+func (c *Chainable[T]) markError(value T, err error) *Chainable[T] {
+	c.sums = value
+	c.lastErrorOperation = c.lastOperation
+	c.lastErrorCaught = err
+	return c
+}
+
+func (c *Chainable[T]) markResult(value T) *Chainable[T] {
+	c.sums = value
+	c.lastSuccessOperation = c.lastOperation
 	return c
 }
--- gocalc_impl.go
+++ gocalc_impl.go
@@ -1,30 +1,56 @@
 package gocalc
 
+// TODO mengimplementasikan pengecekan kesalahan jika terdapat error saat melakukan operasi matematika
+// mungkin berdasarkan dari nilai parameternya atau jenis tipe data
+
 // Operasi penjumlahan
 func (c *Chainable[T]) Plus(n T) IChainable[T] {
-	c.value += n
-	return c
+	c.lastOperation = Plus
+	if c.IsError() {
+		return c
+	}
+	result := c.sums + n
+	return c.markResult(result)
 }
 
 // Operasi pengurangan
 func (c *Chainable[T]) Min(n T) IChainable[T] {
-	c.value -= n
-	return c
+	c.lastOperation = Min
+	if c.IsError() {
+		return c
+	}
+	result := c.sums - n
+	return c.markResult(result)
 }
 
 // Operasi perkalian
 func (c *Chainable[T]) Multiply(n T) IChainable[T] {
-	c.value *= n
-	return c
+	c.lastOperation = Multiply
+	if c.IsError() {
+		return c
+	}
+	result := c.sums * n
+	return c.markResult(result)
 }
 
 // Operasi pembagian
 func (c *Chainable[T]) Divide(n T) IChainable[T] {
-	c.value /= n
-	return c
-}
+	c.lastOperation = Divide
+	if c.IsError() {
+		return c
+	}
+	err := (error)(nil)
+	result := func(err *error) T {
+		defer catch(err)
 
-// Mengambil hasil dari semua opearsi
-func (c Chainable[T]) Sums() T {
-	return c.value
+		if n == 0 {
+			panic("Can't divide number by zero")
+		}
+		return c.sums / n
+	}(&err)
+
+	if err != nil {
+		return c.markError(c.sums, err)
+	}
+	return c.markResult(result)
 }
