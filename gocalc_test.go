af04f991376d130c72934bb080a90bbf26ee87ac
Binar Nugroho <binarnugroho775@gmail.com>
2023-10-24 17:29:36 +0700

    test: update test file

--- gocalc_test.go
+++ gocalc_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 func TestBasicCalculation(t *testing.T) {
 	// Penambahan
-	if c := New(10).Plus(10).Sums(); c != 20 {
+	if c := New(10).Plus(10).Result(); c != 20 {
 		t.Errorf("Expected 20, but got %v", c)
 	}
 
 	// Pengurangan
-	if c := New(100).Min(50).Sums(); c != 50 {
+	if c := New(100).Min(50).Result(); c != 50 {
 		t.Errorf("Expected 50, but got %v", c)
 	}
 
 	// Perkalian
-	if c := New(10).Multiply(10).Sums(); c != 100 {
+	if c := New(10).Multiply(10).Result(); c != 100 {
 		t.Errorf("Expected 100, but got %v", c)
 	}
 
 	// Pembagian
-	if c := New(50).Divide(2).Sums(); c != 25 {
+	if c := New(50).Divide(2).Result(); c != 25 {
 		t.Errorf("Expected 25, but got %v", c)
 	}
 }
@@ -30,7 +30,7 @@ func TestChainableCalculation(t *testing.T) {
 		Multiply(10).
 		Divide(4).
 		Plus(1).
-		Sums(); c != 51 {
+		Result(); c != 51 {
 		t.Errorf("Expected 51, but got %v", c)
 	}
 
@@ -40,7 +40,7 @@ func TestChainableCalculation(t *testing.T) {
 		Multiply(2).
 		Plus(2).
 		Min(1).
-		Sums(); c != 5 {
+		Result(); c != 5 {
 		t.Errorf("Expected 5, but got %v", c)
 	}
 }
@@ -50,7 +50,7 @@ func TestFloatNumberCalculation(t *testing.T) {
 		Divide(2.0).
 		Multiply(10.0).
 		Divide(2.0).
-		Sums(); c != 8.0 {
+		Result(); c != 8.0 {
 		t.Errorf("Expected 8,0, but got %v", c)
 	}
 
@@ -58,7 +58,7 @@ func TestFloatNumberCalculation(t *testing.T) {
 		Multiply(2.0).
 		Multiply(5.0).
 		Divide(2.0).
-		Sums(); c != 500.615 {
+		Result(); c != 500.615 {
 		t.Errorf("Expected 500,615, but got %v", c)
 	}
 
@@ -67,7 +67,7 @@ func TestFloatNumberCalculation(t *testing.T) {
 		Plus(5.0).
 		Multiply(2.0).
 		Min(20.0).
-		Sums(); c != 40.0 {
+		Result(); c != 40.0 {
 		t.Errorf("Expected 20.0, but got %v", c)
 	}
 }
